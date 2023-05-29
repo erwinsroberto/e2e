14a0f85f2f3e921103e7c39ec679b990a7ada7d4
Erwins Roberto <erwinsroberto@gmail.com>
2023-05-29 16:07:07 -0300

    a

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -17,7 +17,7 @@ Cypress.Commands.add('login', (
     cy.get('#email').type(username)
     cy.get('#password').type(password, { log: false })
     cy.contains('button', 'Login').click()
-    cy.contains('h1', 'Your Notes', {timeout: 1000}).should('be.visible')
+    cy.contains('h1', 'Your Notes', {timeout: 3000}).should('be.visible')
   }
 
   if (cacheSession) {
